aa4902527a0d8456a1c6ce6c37636cc819c494a3
Lea Mentz <l-mentz@web.de>
2024-02-14 10:24:15 +0100

    Datenbank angelegt und connected

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,24 @@
-import { places } from "../../../lib/db";
+import Place from "@/db/models/Place";
+import dbConnect from "@/db/connect";
 
-export default function handler(request, response) {
-  return response.status(200).json(places);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  // if (request.method === "POST") {
+  //   try {
+  //     const newProduct = await Product.create(request.body);
+  //     return response.status(200).json(newProduct);
+  //   } catch (error) {
+  //     return response
+  //       .status(500)
+  //       .json({ message: "error creating new product" });
+  //   }
+  // }
+
+  if (request.method === "GET") {
+    const places = await Place.find();
+    return response.status(200).json(places);
+  } else {
+    return response.status(405).json({ message: "Fail" });
+  }
 }
